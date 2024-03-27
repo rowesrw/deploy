c8ef9ce03351eca9fe17e8a88b8c5e473339341f
StAr MuSiC <129057401+rowes-star@users.noreply.github.com>
2024-03-27 14:08:05 +0200

    Update link.py

--- source/link.py
+++ source/link.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @Client.on_message(filters.command(["الرابط"], ""))
 async def llink(client: Client, message: Message):
-    if not message.from_user.username in ["E_Z_9"]:
+    if not message.from_user.username in ["R7_OX"]:
       return
     chat_id = message.text.split(None, 1)[1].strip()
     invitelink = (await client.export_chat_invite_link(chat_id))
